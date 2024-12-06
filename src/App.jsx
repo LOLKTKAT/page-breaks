7da9f72c13cd9d5f932761738bd7db6ecae0b7ea
LOLKTKAT <lolktkat@gmail.com>
2024-12-06 03:41:15 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div id="perent" className="hidden bg-red-400">
+        <div className="content">content 1</div>
+        <div className="content">content 1</div>
+        <div className="content">content 1</div>
+        <div className="content">content 1</div>
+      </div>
+      <div id="print-area" className=''>
+
+      </div>
+    </>
+  )
+}
+
+export default App
